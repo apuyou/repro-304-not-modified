5cc9e5be0a905e35299c59e59507ca8f5acaea85
Arthur Puyou <arthur@puyou.fr>
2022-08-20 17:02:00 +0200

    Add cached pages

---
+++ pages/api/hello-10s.ts
@@ -0,0 +1,17 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: string,
+  time: number
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.setHeader("Cache-Control", "s-maxage=10");
+  res
+    .status(200)
+    .json({ name: "John Doe", time: Math.floor(new Date().getTime() / 1000) });
+}
